4847f14771af2c91f3c06024b17c3fa7470bbbd5
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-04-11 20:39:37 +0430

    make request and validate user data

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,14 +1,22 @@
-import { NextRequest ,NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 
-export async function GET (){
-return NextResponse.json({message: "well done"})
+const issueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function GET() {
+  return NextResponse.json({ message: "well done" });
 }
 
-export async function POST (request: NextRequest){
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
 
-    const body = await request.json()
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  console.log("body: ", body);
 
-    console.log("body: ", body)
-
-    return NextResponse.json({message: "thanks for posting", data: body})
+  return NextResponse.json({ message: "thanks for posting", data: body });
 }
